83f0509ec2f83d6b5c2d6461338e364ab0d4adf4
Francesc Gil <xescugil@gmail.com>
2025-04-04 01:17:45 +0200

    game: Added 'WokeUpTimout' (#22)
    So we can hide the Duck after a while

--- game/store.go
+++ game/store.go
@@ -12,7 +12,7 @@ import (
 type Store struct {
 	*flux.ReduceStore[State, *Action]
 
-	mxLobbies sync.RWMutex
+	mxStore sync.RWMutex
 }
 
 type State struct {
@@ -21,6 +21,9 @@ type State struct {
 	Message          string
 	MessageCreatedAt time.Time
 	MessageTimeout   time.Duration
+
+	WokeUpAt      time.Time
+	WokeUpTimouet time.Duration
 
 	Scale float64
 }
@@ -43,9 +46,14 @@ func NewStore(d *flux.Dispatcher[*Action]) *Store {
 	//a.SetScale(scale, scale)
 
 	s.ReduceStore = flux.NewReduceStore(d, s.Reduce, State{
-		Avatar:         a,
-		Scale:          float64(scale),
+		Avatar: a,
+
+		Scale: float64(scale),
+
 		MessageTimeout: time.Second * 10,
+
+		WokeUpAt:      time.Now(),
+		WokeUpTimouet: time.Second * 15,
 	})
 
 	return s
@@ -54,16 +62,27 @@ func NewStore(d *flux.Dispatcher[*Action]) *Store {
 func (s *Store) Reduce(state State, act *Action) State {
 	switch act.Type {
 	case TPS:
+		s.mxStore.Lock()
+		defer s.mxStore.Unlock()
+
 		// Remove the message if it has been display long enough
 		if state.Message != "" && time.Now().Sub(state.MessageCreatedAt) > state.MessageTimeout {
 			state.Message = ""
 			state.MessageCreatedAt = time.Time{}
 		}
 	case DragAvatar:
+		s.mxStore.Lock()
+		defer s.mxStore.Unlock()
+
 		state.Avatar.SetPosition(float64(act.DragAvatar.X), float64(act.DragAvatar.Y))
 	case AddMessage:
+		s.mxStore.Lock()
+		defer s.mxStore.Unlock()
+
 		state.Message = act.AddMessage.Message
 		state.MessageCreatedAt = time.Now()
+
+		state.WokeUpAt = time.Now()
 	}
 
 	return state
